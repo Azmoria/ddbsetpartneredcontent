738708156d4cd63f625325e7725f493bc640aa83
Azmoria <65363489+Azmoria@users.noreply.github.com>
2024-08-15 11:56:09 -0400

    Update Set DDB partner content filter default on.user.js

--- Set DDB partner content filter default on.user.js
+++ Set DDB partner content filter default on.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Set DDB partner content filter default on
 // @namespace    github.com/azmoria
-// @version      0.2
+// @version      0.3
 // @description  Set default partnered content
 // @author       Azmoria
 // @downloadURL  https://github.com/Azmoria/ddbsetpartneredcontent/raw/main/Set%20DDB%20partner%20content%20filter%20default%20on.user.js
@@ -14,7 +14,23 @@
 
 (function() {
     'use strict';
-     $(`.mm-mega-menu a[href*='/spells'], .mm-mega-menu a[href*='/monsters'], .mm-mega-menu a[href*='/feats'], .mm-mega-menu a[href*='/magic-items']`).each(function(){
-         $(this).attr('href', `${$(this).attr('href')}?filter-partnered-content=t`)
-     });
+    init_links_observe();
 })();
+
+
+function init_links_observe(){
+    let mutation_target = $('#mega-menu-target')[0];
+	let mutation_config = { attributes: false, childList: true, characterData: false, subtree: true };
+
+	let link_observer = new MutationObserver(function() {
+        handle_observe_links()
+	});
+	link_observer.observe(mutation_target,mutation_config);
+
+}
+
+function handle_observe_links() {
+    $(`a[href$='/spells'], a[href$='/monsters'], a[href$='/feats'], a[href$='/magic-items']`).each(function(){
+        $(this).attr('href', `${$(this).attr('href')}?filter-partnered-content=t`)
+    });
+}
